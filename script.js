c36a83bc17c6d40091d04d688e16842cb0940002
gabrielgodoyfnaf4 <gabriel.godoy.oliveira@escola.pr.gov.br>
2026-06-25 13:53:16 +0000

    .

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const curiosidades = [
+    "Golden Freddy pode aparecer aleatoriamente e travar o jogo.",
+    "Scott Cawthon criou FNAF em apenas alguns meses.",
+    "Foxy é o único animatrônico que corre até o escritório.",
+    "As mensagens do Phone Guy escondem pistas importantes da história.",
+    "FNAF 1 foi lançado em agosto de 2014."
+];
 
+const botao = document.getElementById("btnCuriosidade");
+const texto = document.getElementById("textoCuriosidade");
+
+botao.addEventListener("click", () => {
+
+    const indice = Math.floor(
+        Math.random() * curiosidades.length
+    );
+
+    texto.textContent = curiosidades[indice];
+
+});
